23a010d2f68cdfd55041dd6135926ad9f219b159
JMG <jgmore@gmail.com>
2023-12-31 00:33:44 +0000

    fix Copyright

--- d2dcn.py
+++ d2dcn.py
@@ -1,6 +1,6 @@
 # 
 # This file is part of the d2dcn distribution.
-# Copyright (c) 2015 Liviu Ionescu.
+# Copyright (c) 2023 Javier Moreno Garcia.
 # 
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
